0d9ffa96294c3e412f80358f3cede612bb764205
unhingedmagikarp <keller.f.peter@gmail.com>
2023-08-04 10:32:09 +0100

    feat: working slider

---
+++ src/app/components/Slider/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export const Slider = () => {
+  const [sliderPosition, setSliderPosition] = useState(50);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!isDragging) return;
+
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = Math.max(0, Math.min(event.clientX - rect.left, rect.width));
+    const percent = Math.max(0, Math.min((x / rect.width) * 100, 100));
+
+    setSliderPosition(percent);
+  };
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div className="w-full relative" onMouseUp={handleMouseUp}>
+      <div
+        className="relative w-full max-w-[700px] aspect-[70/45] m-auto overflow-hidden select-none"
+        onMouseMove={handleMove}
+        onMouseDown={handleMouseDown}
+      >
+        <Image
+          alt=""
+          fill
+          priority
+          src="https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80"
+        />
+
+        <div
+          className="absolute top-0 left-0 right-0 w-full max-w-[700px] aspect-[70/45] m-auto overflow-hidden select-none"
+          style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
+        >
+          <Image
+            fill
+            priority
+            alt=""
+            src="https://images.unsplash.com/photo-1598875791852-8bb153e713f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2010&q=80"
+          />
+        </div>
+        <div
+          className="absolute top-0 bottom-0 w-1 bg-white cursor-ew-resize"
+          style={{
+            left: `calc(${sliderPosition}% - 1px)`,
+          }}
+        >
+          <div className="bg-white absolute rounded-full h-3 w-3 -left-1 top-[calc(50%-5px)]" />
+        </div>
+      </div>
+    </div>
+  );
+};
